bc96feaaf5c9a10548b2f601d7e44c5959ab98c3
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2025-02-19 13:47:32 -0300

    Estudos do tailwind

--- home/models.py
+++ home/models.py
@@ -4,4 +4,8 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    def get_context(self, request):
+        # Update context to include only published posts, ordered by reverse-chron
+        context = super().get_context(request)
+        context['loop_times'] = range(0, 60)
+        return context
