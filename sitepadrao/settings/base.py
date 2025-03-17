ca831774cd9b6b309cfd04a5b05dcff78e6aa134
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2025-03-17 12:37:31 -0300

    Ajuste nos STATICFILES_DIRS

--- sitepadrao/settings/base.py
+++ sitepadrao/settings/base.py
@@ -156,7 +156,7 @@ STATICFILES_FINDERS = [
 
 STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "static"),
-    os.path.join(BASE_DIR, 'frontend'),
+    os.path.join(BASE_DIR, 'frontend/bundles'),
 ]
 
 WEBPACK_LOADER = {
