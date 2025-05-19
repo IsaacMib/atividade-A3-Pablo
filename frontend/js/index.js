c3c28f89599934d08b9eeffa520ed6e2060ae9e0
Carlos André Martins da Silva <carlos.andre@codata.pb.gov.br>
2025-05-19 13:40:29 -0300

    Ajustes no Swiper + Melhorias na estilização do card

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -12,8 +12,10 @@ import Icons from 'uikit/dist/js/uikit-icons';
 import './header/header.js';
 
 import Swiper from 'swiper/bundle';
-
 import 'swiper/css/bundle';
+
+// Make Swiper available globally
+window.Swiper = Swiper;
 
 // loads the Icon plugin
 UIkit.use(Icons);
@@ -31,31 +33,4 @@ document.addEventListener("DOMContentLoaded", function() {
       window.barraIdentidade.closeAllToggleGoverno();
     }
   });
-});
-
-const swiper = new Swiper(".swiperServicosOnline", {
-  slidesPerView: 1,
-  spaceBetween: 2,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    640: {
-      slidesPerView: 1,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 50,
-    },
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
