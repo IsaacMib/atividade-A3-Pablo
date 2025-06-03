8b085dc51dab09c621346c183c13b9a1d28a7956
Makoby Lopes <makobylopes@codata.pb.gov.br>
2025-06-03 10:40:22 -0300

    resolvendo conflitos da feat/back_titulo fazendo a feat/backand_titulo

---
+++ blocks/models/makoby.py
@@ -0,0 +1,25 @@
+from wagtail import blocks
+
+
+class TituloBlock(blocks.StructBlock):
+   """Bloco de título com opções de estilo e visibilidade."""
+   titulo = blocks.CharBlock(
+       required=True,
+       help_text='Digite o título que será exibido'
+   )
+   bgAzul = blocks.BooleanBlock(
+       required=False,
+       default=False,
+       help_text='Marque para usar fundo azul com texto branco. Deixe desmarcado para texto azul com fundo branco.'
+   )
+   '''
+   visivel = blocks.BooleanBlock(
+       required=False,
+       default=True,
+       help_text='Desmarque para ocultar o título'
+   )
+   '''
+   class Meta:
+       template = 'blocks/titulo.html'
+       icon = 'title'
+       label = 'Título'
