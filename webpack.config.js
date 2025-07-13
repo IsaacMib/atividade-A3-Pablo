4a0eea2a9fb2048006798824454321b5b2311574
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2025-07-13 20:29:52 -0300

    Melhoria para não gerar tanto arquivo CSS em ambiente de dev, e adicionado minificação em ambinete de produção, retirado variáveis de ambinete do build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const BundleTracker = require("webpack-bundle-tracker");
 const SpritesmithPlugin = require('webpack-spritesmith');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
     entry: ["./frontend/js/index.js"], // Arquivo de entrada
@@ -10,58 +13,71 @@ module.exports = {
         path: path.resolve("./frontend/bundles/"),
         filename: "[name].js",
     },
-    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+    mode: isProduction ? "production" : "development",
     optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()], // Minificação do JS
+        minimize: true,
+        minimizer: [
+            new TerserPlugin(),
+            ...(isProduction ? [new CssMinimizerPlugin({
+                minimizerOptions: {
+                    preset: [
+                        "default",
+                        {
+                            discardComments: { removeAll: true },
+                        },
+                    ],
+                },
+            })] : []), // Adiciona CssMinimizerPlugin apenas no modo produção
+        ],
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: "styles.[contenthash].css" }), // Para arquivos CSS, se necessário,
-      new BundleTracker({ path: __dirname, filename: "webpack-stats.json" }),
-      new SpritesmithPlugin({
-        src: {
-            cwd: `./frontend/sprites-img`,
-            glob: '*.png'
-          },
-          target: {
-            image: `./frontend/sprite/sprite.png`,
-            css: `./frontend/scss/sprite.scss`
-          },
-          apiOptions: {
-            cssImageRef: '/frontend/sprite/sprite.png'
-          }
-      })
+        new MiniCssExtractPlugin({
+            filename: isProduction ? "styles.[contenthash].css" : "styles.css"
+        }),
+        new BundleTracker({ path: __dirname, filename: "webpack-stats.json" }),
+        new SpritesmithPlugin({
+            src: {
+                cwd: `./frontend/sprites-img`,
+                glob: '*.png'
+            },
+            target: {
+                image: `./frontend/sprite/sprite.png`,
+                css: `./frontend/scss/sprite.scss`
+            },
+            apiOptions: {
+                cssImageRef: '/frontend/sprite/sprite.png'
+            }
+        })
     ],
     module: {
-      rules: [
-          {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: {
-                  loader: "babel-loader",
-                  options: {
-                      presets: ["@babel/preset-env"],
-                  },
-              },
-          },
-          {
-            test: /\.s[ac]ss$/i, // Processa arquivos .scss e .sass
-            use: [
-                //process.env.NODE_ENV === "production" ? MiniCssExtractPlugin.loader : "style-loader",
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
-            ],
-          },
-          {
-            test: /\.css$/i,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "sass-loader",
-                'postcss-loader',
-            ],
-          },
-      ],
-  },
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                    'postcss-loader',
+                ],
+            },
+        ],
+    },
 };
