e7a14550c17e067e9224ffccb0d9222dcc6d3e2f
Andre Oliveira <andre.oliveira@codata.pb.gov.br>
2025-09-17 14:21:27 -0300

    Adapta widget para considerar o valor máximo definido no campo quando definido

--- frontend/js/char-count-controller.js
+++ frontend/js/char-count-controller.js
@@ -1,42 +1,45 @@
 class CharCountController extends window.StimulusModule.Controller {
-    static values = { max: { default: 220, type: Number } };
+  static values = { max: { default: 220, type: Number } };
 
-    connect() {
-        console.log("CharCountController");
-        this.setupOutput();
-        this.updateCount();
-    }
+  connect() {
+    this.setupOutput();
+    this.updateCount();
+    this.definedMaxValue = this.maxValue;
+  }
 
-    setupOutput() {
-        if (this.output) return;
-        const template = document.createElement('template');
-        template.innerHTML = `<div class="charcount-wrapper" style="display: flex; justify-content: flex-end;">
+
+  setupOutput() {
+    if (this.output) return;
+    const maxValue = this.element.getAttribute('maxlength');
+    this.definedMaxValue = maxValue ?? this.maxValue;
+    const template = document.createElement('template');
+    template.innerHTML = `<div class="charcount-wrapper" style="display: flex; justify-content: flex-end;">
             <output
                 name='char-count'
                 for='${this.element.id}'
                 class='output-label'
             ></output>
         </div>`;
-        const output = template.content.firstChild;
-        this.element.insertAdjacentElement('afterend', output);
-        this.output = output;
-    }
+    const output = template.content.firstChild;
+    this.element.insertAdjacentElement('afterend', output);
+    this.output = output;
+  }
 
-    updateCount(event) {
-        const value = event ? event.target.value : this.element.value;
-        const chars = (value || '').length;
-        if (chars >= this.maxValue) {
-            this.output.style.color = "red";
-        } else if (chars > this.maxValue * 0.8) {
-            this.output.style.color = "orange";
-        } else {
-            this.output.style.color = "inherit";
-        }
-        this.output.textContent = `${chars} / ${this.maxValue} caracteres`;
+  updateCount(event) {
+    const value = event ? event.target.value : this.element.value;
+    const chars = (value || '').length;
+    if (chars >= this.definedMaxValue) {
+      this.output.style.color = 'red';
+    } else if (chars > this.definedMaxValue * 0.8) {
+      this.output.style.color = 'orange';
+    } else {
+      this.output.style.color = 'inherit';
     }
+    this.output.textContent = `${chars} / ${this.definedMaxValue} caracteres`;
+  }
 
-    disconnect() {
-        this.output && this.output.remove();
-    }
+  disconnect() {
+    this.output && this.output.remove();
+  }
 }
 window.wagtail.app.register('char-count', CharCountController);
