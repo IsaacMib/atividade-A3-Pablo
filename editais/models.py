970e9bc597324d23d7014ec307e40f848617b4e2
otacio.neto <otacio.neto@codata.pb.gov.br>
2025-10-14 10:20:44 -0300

    Adiciona o arquivo blocos e ajusta o models

--- editais/models.py
+++ editais/models.py
@@ -5,58 +5,10 @@ from wagtail.fields import StreamField, RichTextField
 from wagtail.contrib.routable_page.models import RoutablePageMixin
 
 from core.models import PageSitePadrao, PageSitePadraoIndex
-from blocks.models import EspecificDocumentChooserBlock
-
 from datetime import datetime
 from collections import defaultdict
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-from wagtail.blocks import (
-    CharBlock,
-    RichTextBlock,
-    StructBlock,
-    ListBlock,
-    DateTimeBlock,
-    URLBlock,
-    StreamBlock,
-)
-
-
-SITUACAO_EDITAL_CHOICES = [
-    ('aberto', 'Aberto'),
-    ('em_andamento', 'Em Andamento'),
-    ('encerrado', 'Encerrado'),
-    ('suspenso', 'Suspenso'),
-    ('cancelado', 'Cancelado'),
-]
-
-
-class AnexoChoiceBlock(StreamBlock):
-    arquivo = StructBlock([
-        ('nome_documento', CharBlock(required=True, label="Nome para exibição do arquivo")),
-        ('documento', EspecificDocumentChooserBlock(required=True, label="Arquivo")),
-    ], label="Arquivo", icon="doc-full-inverse")
-
-    link_externo = StructBlock([
-        ('nome_link', CharBlock(required=True, label="Nome para exibição do link")),
-        ('url', URLBlock(required=True, label="URL do link externo")),
-    ], label="Link Externo", icon="link")
-
-    class Meta:
-        label = "Anexo"
-        max_num = 1
-
-
-class FaseEditalBlock(StructBlock):
-    titulo_fase = CharBlock(required=True, label="Título da Fase")
-    data_fase = DateTimeBlock(required=True, label="Data e Hora da Fase")
-    anexo = AnexoChoiceBlock(required=False)
-
-    class Meta:
-        icon = 'date'
-        label = "Fase do Edital"
-        template = 'fase_edital_block.html'
-
+from .blocks import FaseEditalBlock, SITUACAO_EDITAL_CHOICES
 
 class EditalPage(PageSitePadrao):
     numero = models.CharField(max_length=50, verbose_name="Número do Edital")
