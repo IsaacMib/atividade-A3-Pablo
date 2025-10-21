8e78b50cadcbc46ff9e339ccaa94489361302ec2
Andre Oliveira <andre.oliveira@codata.pb.gov.br>
2025-10-21 13:22:16 -0300

    Pequena mudança no alerta de divergência na contagem de carácteres

--- frontend/js/char-count-controller.js
+++ frontend/js/char-count-controller.js
@@ -44,8 +44,8 @@ class CharCountController extends window.StimulusModule.Controller {
       if (maxLength === undefined) {
         maxLength = field.getAttribute('maxlength');
       }
-      else if (field.getAttribute('maxlength') !== maxLength) {
-        console.log('O limite de caracteres do campo ' + field.name + ' já havia sido estabelecido e não coincide com o valor definido no controller.');
+      else if (field.getAttribute('maxlength') != maxLength) {
+        console.log(`O limite de caracteres do campo ${field.name} já havia sido estabelecido em ${field.getAttribute('maxlength')} e não coincide com o valor definido no controller ${maxLength}.`);
       }
     }
     field.setAttribute('maxlength', maxLength);
