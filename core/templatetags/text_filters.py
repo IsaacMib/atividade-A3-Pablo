7544bc020c9980fe204094bdc6fb1e4a372a01cc
Andre Oliveira <andre.oliveira@codata.pb.gov.br>
2025-10-21 12:49:20 -0300

    Adiciona link 'Ver mais' ao final do texto e deixa todo o card clicável

--- core/templatetags/text_filters.py
+++ core/templatetags/text_filters.py
@@ -6,5 +6,9 @@ register = template.Library()
 
 
 @register.filter(name='add_link_to_trimmed_text')
-def add_link_to_trimmed_text(text_length, link_path):
-    return mark_safe(format_html('<a href="{}">Ver mais</a>', link_path))
+def add_link_to_trimmed_text(text_content, link_path):
+    return mark_safe(format_html(
+        '<p lang="pt-BR" class="card-linha-do-tempo__description">{}<a href="{}" class="ver-mais-link">Ver mais</a></p>',
+        text_content,
+        link_path
+    ))
