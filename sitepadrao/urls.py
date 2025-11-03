ed9cc599a711e849acc0255a3aee3c60905fd931
isaac.menezes <isaac.menezes@codata.pb.gov.br>
2025-11-03 10:33:01 -0300

    Separei o visualizador de documentos genérico para o core, permitindo reutilização em outros apps além de "documentos". Adicionei também um visualizador alternativo para casos onde o iframe não funciona (ex: Chrome com PDF).

--- sitepadrao/urls.py
+++ sitepadrao/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path("health/", sitepadrao_views.health_check, name='health_check'),
     path("django-admin/", admin.site.urls),
     path("documents/", include(wagtaildocs_urls)),
+    path("core/", include("core.urls")),  # URLs do core (document viewer e outros)
     path("docs/", include("documentos.urls")),  # URLs customizadas para documentos
     re_path(
         r"^images/([^/]*)/(\d*)/([^/]*)/[^/]*$",
