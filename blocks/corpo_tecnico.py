f544ceba08ffc83ef4a1b46de4cfe2bb6f621a02
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2025-11-03 11:02:15 -0300

    Altera para CorpoTecnico Seja abstrato no models de paginas.

--- blocks/corpo_tecnico.py
+++ blocks/corpo_tecnico.py
@@ -12,8 +12,10 @@ from django.core.exceptions import ValidationError
 
 class ListGrupoCorpoTecnicoBlock(StructBlock):
     titulo = CharBlock(required=False, max_length=255, label="Título do Grupo")
-    grupo_page = PageChooserBlock(
-        target_model="paginas.CorpoTecnicoGrupoPageIndex",
-        required=True,
-        label="Selecione a página do grupo"
-    )
+    # PageChooserBlock comentado pois referencia classe abstrata
+    # Deve ser reimplementado nas apps que usarem as classes concretas
+    # grupo_page = PageChooserBlock(
+    #     target_model="paginas.CorpoTecnicoGrupoPageIndex",
+    #     required=True,
+    #     label="Selecione a página do grupo"
+    # )
