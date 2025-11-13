6b1a03da2d1524a27e5e1270914e8c1dfaa712f3
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2025-11-13 13:55:09 -0300

    chore: limpa paginas_codata e adiciona diretórios de teste ao gitignore
    Alterações:
    - paginas_codata/models.py: Removidos imports não utilizados
      (arquivo completamente vazio agora - app não usado no projeto)
    
    - .gitignore: Adiciona diretórios criados por testes
      * test-media/ - Criado quando testes usam Image do Wagtail
      * test-static/ - Criado pelo settings/testing.py
    
    Contexto:
    - test-media/ foi criado durante execução de testes de avisos
    - Contém apenas arquivos temporários de teste (test.png)
    - Não deve ser versionado no repositório

--- paginas_codata/models.py
+++ paginas_codata/models.py
@@ -1,34 +1,3 @@
-from datetime import datetime
 from django.db import models
-from wagtail.models import Page
-from django.utils.text import slugify
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.core.exceptions import ValidationError
-from django.contrib import messages
-from django.shortcuts import redirect, render
 
-from wagtail.fields import StreamField
-from wagtail.search import index
-from wagtail.contrib.routable_page.models import RoutablePageMixin, route
-from wagtail.admin.panels import (
-    ObjectList, FieldPanel, MultiFieldPanel, TabbedInterface
-)
-from wagtail.images.blocks import ImageChooserBlock
-
-from modelcluster.fields import ParentalKey
-from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import Tag, TaggedItemBase
-
-from core.models import PageSitePadrao, PageSitePadraoIndex
-from blocks.models import BaseStreamBlock, EspecificDocumentChooserBlock
-from core.utils import (
-    get_file_type,
-    get_fontawesome_file_icon,
-    get_page_title_with_counter,
-    get_widget_input_with_counter
-)
-
-# Importar classe base abstrata centralizada em paginas.models
-# (evita duplicação de código)
-from paginas.models import AvisosDefaultPage
-
+# Create your models here.
