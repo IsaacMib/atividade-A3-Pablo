8034255e52b4bf15f796683690a9fd62124ed342
thiago.oliveira <thiago.oliveira@codatsa.pb.gov.br>
2025-11-14 13:08:21 -0300

    Criando Sub Paginas de Comissão Etica e Missão Visão e Valores

--- lgpd/models.py
+++ lgpd/models.py
@@ -11,3 +11,20 @@ class LGPDPage(PaginaComBannerPage):
     class Meta:
         verbose_name = "LGPD"
         verbose_name_plural = "LGPD"
+
+
+class MissaoVisaoValores(PaginaComBannerPage):
+    template = "paginas/pagina_com_banner_page.html"
+
+    class Meta:
+        verbose_name = "Missão, Visão e Valores"
+        verbose_name_plural = "Missão, Visão e Valores"
+
+
+class ComissaoEtica(PaginaComBannerPage):
+    template = "paginas/pagina_com_banner_page.html"
+
+    class Meta:
+        verbose_name = "Comissão de Ética"
+        verbose_name_plural = "Comissões de Ética"
+
