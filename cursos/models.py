fa1772894f1a31315a57202a02e43caa9a8c1fd8
isaac.menezes <isaac.menezes@codata.pb.gov.br>
2025-11-14 11:54:50 -0300

    feat: restrict parent page types for CursosIndexPage to only TreinamentoIndexPage

--- cursos/models.py
+++ cursos/models.py
@@ -192,7 +192,7 @@ class CursosIndexPage(RoutablePageMixin, PageSitePadraoIndex):
         FieldPanel("introduction"),
     ]
 
-    parent_page_types = ["home.HomePage", "treinamento.TreinamentoIndexPage"]
+    parent_page_types = ["treinamento.TreinamentoIndexPage"]
     subpage_types = ["CursosPage"]
 
     def get_context(self, request):
