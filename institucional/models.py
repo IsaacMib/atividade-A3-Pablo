d7549bc4b41b97e857375fad458fbb7ad9e11434
thiago.oliveira <thiago.oliveira@codatsa.pb.gov.br>
2025-11-17 08:35:57 -0300

    Mudando a class MissaoVisaoValores, para app de Intituição

--- institucional/models.py
+++ institucional/models.py
@@ -120,3 +120,14 @@ class AEmpresaPage(RichTextPage):
     class Meta:
         verbose_name = "Página A Empresa"
         verbose_name_plural = "Páginas A Empresa"
+
+
+class MissaoVisaoValores(PaginaComBannerPage):
+    template = "paginas/pagina_com_banner_page.html"
+
+    parent_page_types = [ 'institucional.InstitucionalIndexPage' ]
+    subpage_types = []
+
+    class Meta:
+        verbose_name = "Missão, Visão e Valores"
+        verbose_name_plural = "Missão, Visão e Valores"
--- lgpd/models.py
+++ lgpd/models.py
@@ -13,12 +13,4 @@ class LGPDPage(PaginaComBannerPage):
         verbose_name_plural = "LGPD"
 
 
-class MissaoVisaoValores(PaginaComBannerPage):
-    template = "paginas/pagina_com_banner_page.html"
-
-    class Meta:
-        verbose_name = "Missão, Visão e Valores"
-        verbose_name_plural = "Missão, Visão e Valores"
-
-
 
