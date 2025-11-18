c7743adcb22a95019a5a4ec89e84717e5124afda
Andre Oliveira <andre.oliveira@codata.pb.gov.br>
2025-11-18 13:07:11 -0300

    Adiciona widget para habilitar ou desabilitar campos de adição de links no bloco da home da linha do tempo

--- core/wagtail_hooks.py
+++ core/wagtail_hooks.py
@@ -1,5 +1,3 @@
-from django.urls import reverse
-from django.conf import settings
 from wagtail import hooks
 from agenda.models import AgendaIndexPage
 from noticias.models import NoticiasIndexPages
@@ -13,21 +11,36 @@ from django.utils.html import format_html_join
 
 @hooks.register('before_create_page')
 def do_before_agendaindex_page_edit(request, parent_page, page_class):
-    tipos_unicos = [AgendaIndexPage, AvisosIndexPage, NoticiasIndexPages, ContatosPage]
+    tipos_unicos = [
+        AgendaIndexPage,
+        AvisosIndexPage,
+        NoticiasIndexPages,
+        ContatosPage,
+    ]
     if page_class in tipos_unicos:
         # Verifica se já existe um filho do mesmo tipo para o parent_page
         if parent_page.get_children().type(page_class).exists():
             messages.error(
                 request,
-                f"Já existe uma página do tipo {page_class._meta.verbose_name} para este local. Só é permitido um por pai."
+                (
+                    "Já existe uma página do tipo "
+                    f"{page_class._meta.verbose_name} para este local. "
+                    "Só é permitido um por pai."
+                )
             )
-            return redirect("wagtailadmin_explore", parent_page_id=parent_page.id)
+            return redirect(
+                "wagtailadmin_explore",
+                parent_page_id=parent_page.id,
+            )
 
 
 @hooks.register('insert_editor_js')
 def editor_js():
-    # Adicionar arquivo js para contagem de caracteres
-    js_files = ['js/char-count-controller.js',]
+    # Adicionar arquivos js para contagem de caracteres e controles adicionais
+    js_files = [
+        'js/char-count-controller.js',
+        'js/card-links-controller.js',
+    ]
     return format_html_join('\n', '<script src="{0}"></script>',
                             ((static(filename),) for filename in js_files)
                             )
